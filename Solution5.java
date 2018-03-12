2e7fec2c65bd660ae2d0af0e4cf290dd1093c68b
NGUYEN NGO HOANG LOC <locnnhjp19292@gmail.com>
2018-03-13 00:52:43 +0900

    Solution5.java

---
+++ Solution5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String B = sc.next();
+		System.out.println(A.length() + B.length());
+		if(A.compareTo(B) >= 0) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+		String newA = A.substring(0, 1).toUpperCase() + A.substring(1, A.length());
+		String newB = B.substring(0, 1).toUpperCase() + B.substring(1, B.length());
+		System.out.println(newA + " " + newB);
+	}
+}
